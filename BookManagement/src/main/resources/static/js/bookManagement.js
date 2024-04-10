8e34d56c554aa953a5c0eae7b0bb682fd74d98e1
kimare <kimarv@naver.com>
2024-04-10 20:34:55 +0900

    도서관리 숙제

--- BookManagement/src/main/resources/static/js/bookManagement.js
+++ BookManagement/src/main/resources/static/js/bookManagement.js
@@ -32,6 +32,7 @@ if(bookListTable !=null){
     })
   });
 
+  
 }
 
 
@@ -41,16 +42,18 @@ if(bookListTable !=null){
 const searchBookBtn = document.querySelector("#searchBookBtn");
 const inputTitle = document.querySelector("#inputTitle");
 const bookTitleSearch = document.querySelector("#bookTitleSearch");
-const serchBookTbody = document.querySelector("#serchBookTbody");
+const searchBookTbody = document.querySelector("#searchBookTbody");
+const btnMod = document.querySelectorAll(".btnMod");
+const btnDel = document.querySelector(".btnDel");
 
 if(bookTitleSearch != null){
 
   searchBookBtn.addEventListener("click",()=>{
+    //fnSearch();
 
     const param = {
       "bookTitle" : inputTitle.value
     }
-  
     fetch("/bookSearch/bookTitleSearch",{
       method:"POST",
       headers:{"Content-Type":"application/json"},
@@ -60,33 +63,62 @@ if(bookTitleSearch != null){
     .then(result=>{
       const bookSearchList = JSON.parse(result);
       //console.log(bookSearchList);
-      serchBookTbody.innerText = "";
+      searchBookTbody.innerText = "";
 
       for(let book of bookSearchList){
-        //console.log(book);
+        //console.log(book.bookNo);
         const thead = document.querySelector("#thead");
-        const th = thead.querySelectorAll("th");
+        //const th = thead.querySelectorAll("th");
         const tr = document.createElement("tr");
-        
-        for(let i=0 ;i<th.length;i++){
-          
 
-
-
+        const arr = ['bookNo','bookTitle','bookWriter','bookPrice','regDate'];
+        for(let i = 0; i < arr.length; i++) {
+          tr.append(createTd(book[arr[i]]));
         }
-
+        tr.append(createBtn(book["bookNo"], "btnMod"));
+        tr.append(createBtn(book["bookNo"], "btnDel"));
+        searchBookTbody.append(tr);
       }
-      
+
+      fnMod();
     })
+
   });
 };
 
+function fnMod()
+{
+   const btnMod = document.querySelectorAll(".btnMod");
+    btnMod.forEach(button => {
+      //console.log(">>>>>>>>>>>> "+button)
+      let key = button.name; // 글번호
+      button.addEventListener('click', ()=>{
+        console.log("key >>>>>>>>>>>> "+ key);
+        // 수정
+        //fnMod()
 
+      }, false);
+  });
+}
   
 
 //td 함수
 const createTd = (text)=>{
   const td = document.createElement("td");
   td.innerText = text;
+  return td;
+}
+
+//td 함수
+const createBtn = (text, btnClass)=>{
+  const td = document.createElement("td");
+  let val = "";
+  if (btnClass == "btnMod") {
+    val = "수정";
+  } else {
+    val = "삭제";
+  }
+
+  td.innerHTML = "<input type = 'button' value='"+ val +"' class='"+ btnClass +"' name='"+ text +"'/ >";
   return td;
 }
